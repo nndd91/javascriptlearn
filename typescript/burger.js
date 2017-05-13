35ed7dff87e3e15f03763fbde6429a555d2dbd45
nndd91 <nndd91@hotmail.com>
2017-05-13 22:54:46 +0800

    ADded typescript

---
+++ typescript/burger.js
@@ -0,0 +1,5 @@
+var burger = 'hamburger', calories = 300, tasty = true;
+function speak(food, energy) {
+    console.log("Our " + food + " has " + energy + "calories.");
+}
+speak(burger, calories);
